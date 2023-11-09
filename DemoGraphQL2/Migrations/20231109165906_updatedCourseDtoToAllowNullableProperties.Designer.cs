3ccfaa78139004bd8bc6defd99e08d6ea7ede6c4
Gabriel Canosa <g.canosa90@gmail.com>
2023-11-09 17:59:49 +0100

    Updated CourseDto To Allow Nullable Properties

--- DemoGraphQL2/Migrations/20231109165906_updatedCourseDtoToAllowNullableProperties.Designer.cs
+++ DemoGraphQL2/Migrations/20231109165906_updatedCourseDtoToAllowNullableProperties.Designer.cs
@@ -4,6 +4,7 @@ using DemoGraphQL2.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DemoGraphQL2.Migrations
 {
     [DbContext(typeof(SchoolDbContext))]
-    partial class SchoolDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231109165906_updatedCourseDtoToAllowNullableProperties")]
+    partial class updatedCourseDtoToAllowNullableProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
